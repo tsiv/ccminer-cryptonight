bedaf007d4619fc4157aeafb59b44850f08d93f1
KlausT <kto@gmx.de>
2017-10-26 07:23:15 +0200

    version 2.06beta

--- cpu-miner.c
+++ cpu-miner.c
@@ -1891,7 +1891,7 @@ static int msver(void)
 	return version;
 }
 
-#define PROGRAM_VERSION "2.05"
+#define PROGRAM_VERSION "2.06beta"
 int main(int argc, char *argv[])
 {
 	struct thr_info *thr;
--- cpuminer-config.h
+++ cpuminer-config.h
@@ -152,7 +152,7 @@
 #define PACKAGE_NAME "ccminer-cryptonight"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "ccminer-cryptonight 2.05"
+#define PACKAGE_STRING "ccminer-cryptonight 2.06beta"
 
 /* Define to the one symbol short name of this package. */
 #undef PACKAGE_TARNAME
@@ -161,7 +161,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.05"
+#define PACKAGE_VERSION "2.06beta"
 
 /* Version number of package */
 #undef VERSION
